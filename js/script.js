cfc3d46ee8abc50f10247049a046832bc8bad121
Marial Clarie <marialclarie@gmail.com>
2021-07-30 19:19:43 +0300

    Displayed clients output elements in jquerry

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
-
+        var uName= $('#name').val();
+        var pSize= $('#pizzaSize').val();
+        var crust=$('#crust').val();
+        var noOfPizza=$('#noOfPizza').val()
+        var delivery=$('#delivery').val()
+        var pick=$('#pick').val()
 
 $(document).ready(function(){
+    var uname=$('#name').val();
+        
     //Show delivery form
     $('#delivery').click(function(){
-        $('.del-details').show()
+        $('.del-details').show();
     })
     // Hide delivery Form
     $('#pick').click(function(){
-        $('.del-details').hide()
+        $('.del-details').hide();
     })
     // Display input
-    $('.cinputs').submit(function(){
-        var uname=$('#name').val()
-        var pSize= $('#pizzaSize').val()
-        var crust=$('')
-        alert(uname+pSize)
-    })
+    $('.cinputs').submit(function(event){
+        
 
+        //event.preventDefault();
+        
+
+    })
+    $('#checkdetails').click(function(){
+     
+        
+        $('#nameOut').append("Dear"+uName+('\n'))
+        $('#sizeOut').append("You Ordered "+pSize)
+        $('#crustOut').append("You Ordered "+crust)
+      
+    })
+    
 
 
    
