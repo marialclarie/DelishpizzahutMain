2fce5d819a51a3fa4969f6e10ec9b66bfe63b64c
Marial Clarie <marialclarie@gmail.com>
2021-07-30 18:13:17 +0300

    Decalred clients input elements in jquerry

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
+
+
 $(document).ready(function(){
-    //Show contact form
+    //Show delivery form
     $('#delivery').click(function(){
         $('.del-details').show()
     })
+    // Hide delivery Form
     $('#pick').click(function(){
         $('.del-details').hide()
     })
+    // Display input
+    $('.cinputs').submit(function(){
+        var uname=$('#name').val()
+        var pSize= $('#pizzaSize').val()
+        var crust=$('')
+        alert(uname+pSize)
+    })
+
+
 
    
 })
