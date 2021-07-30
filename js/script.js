d61630f56e7f05d18c49eb5eff21d38fb62c3556
Marial Clarie <marialclarie@gmail.com>
2021-07-30 17:52:03 +0300

    Added hide and show jquerry comdand for delivery details

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+    //Show contact form
     $('#delivery').click(function(){
         $('.del-details').show()
     })
     $('#pick').click(function(){
         $('.del-details').hide()
     })
+
    
 })
